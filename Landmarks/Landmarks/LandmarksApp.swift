c2e0c9caa8245d5fc699c50ad5b4ffb1e3b59cc3
ingridsilapan <xerome-mobile@xero.com>
2023-03-10 21:58:56 +1300

    Add Landmarks project for creating and combining views

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ingrid Silapan on 10/03/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
